fc0c0e1cfb0832aedf1b3ab401ac74739cc474e4
hagansamuel6 <hagansamuel6@gmail.com>
2022-11-29 17:42:21 +0000

    material ui integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,185 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import Button from '@mui/material/Button';
 
-function App() {
-  // const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <div className="">
-     <h2>kljlkjl;</h2>
-    </div>
-  )
-}
+    <div className="flex">
+      <nav className="max-h-screen w-80 p-6 bg-blue-600 text-white overflow-auto hidden lg:block" >
+        <div className="h-16 w-full bg-orange-400 "></div>
 
-export default App
+        <Button variant="contained">Hello World</Button>
+        
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In nisi ex
+        eaque similique! Harum laudantium reiciendis placeat accusantium ab
+        asperiores perferendis cupiditate? Delectus debitis, ipsa molestias
+        accusamus iusto aliquam porro. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. In nisi ex eaque similique! Harum laudantium
+        reiciendis placeat accusantium ab asperiores perferendis cupiditate?
+        Delectus debitis, ipsa molestias accusamus iusto aliquam porro. Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. In nisi ex eaque
+        similique! Harum laudantium reiciendis placeat accusantium ab asperiores
+        perferendis cupiditate? Delectus debitis, ipsa molestias accusamus iusto
+        aliquam porro. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        In nisi ex eaque similique! Harum laudantium reiciendis placeat
+        accusantium ab asperiores perferendis cupiditate? Delectus debitis, ipsa
+        molestias accusamus iusto aliquam porro. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. In nisi ex eaque similique! Harum
+        laudantium reiciendis placeat accusantium ab asperiores perferendis
+        cupiditate? Delectus debitis, ipsa molestias accusamus iusto aliquam
+        porro. Lorem ipsum dolor sit amet consectetur adipisicing elit. In nisi
+        ex eaque similique! Harum laudantium reiciendis placeat accusantium ab
+        asperiores perferendis cupiditate? Delectus debitis, ipsa molestias
+        accusamus iusto aliquam porro. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. In nisi ex eaque similique! Harum laudantium
+        reiciendis placeat accusantium ab asperiores perferendis cupiditate?
+        Delectus debitis, ipsa molestias accusamus iusto aliquam porro. Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. In nisi ex eaque
+        similique! Harum laudantium reiciendis placeat accusantium ab asperiores
+        perferendis cupiditate? Delectus debitis, ipsa molestias accusamus iusto
+        aliquam porro. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        In nisi ex eaque similique! Harum laudantium reiciendis placeat
+        accusantium ab asperiores perferendis cupiditate? Delectus debitis, ipsa
+        molestias accusamus iusto aliquam porro. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. In nisi ex eaque similique! Harum
+        laudantium reiciendis placeat accusantium ab asperiores perferendis
+        cupiditate? Delectus debitis, ipsa molestias accusamus iusto aliquam
+        porro. Lorem ipsum dolor sit amet consectetur adipisicing elit. In nisi
+        ex eaque similique! Harum laudantium reiciendis placeat accusantium ab
+        asperiores perferendis cupiditate? Delectus debitis, ipsa molestias
+        accusamus iusto aliquam porro.
+      </nav>
+
+      <div className="flex-1 max-h-screen top-0 right-0 overflow-auto relative">
+        <header className="fixed h-16 w-full bg-green-600 p-6 mb-6">
+          <div className="bg-gray-500">header</div>
+        </header>
+        <main className=" w-full bg-red-500 p-6 mt-12">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
+          numquam delectus voluptatum inventore. Quod eligendi, harum libero
+          eveniet totam vel aut reiciendis quidem necessitatibus, itaque
+          delectus voluptatem numquam aliquid perferendis! Lorem ipsum dolor sit
+          amet, consectetur adipisicing elit. Reiciendis numquam delectus
+          voluptatum inventore. Quod eligendi, harum libero eveniet totam vel
+          aut reiciendis quidem necessitatibus, itaque delectus voluptatem
+          numquam aliquid perferendis! Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Reiciendis numquam delectus voluptatum inventore.
+          Quod eligendi, harum libero eveniet totam vel aut reiciendis quidem
+          necessitatibus, itaque delectus voluptatem numquam aliquid
+          perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Reiciendis numquam delectus voluptatum inventore. Quod eligendi, harum
+          libero eveniet totam vel aut reiciendis quidem necessitatibus, itaque
+          delectus voluptatem numquam aliquid perferendis! Lorem ipsum dolor sit
+          amet, consectetur adipisicing elit. Reiciendis numquam delectus
+          voluptatum inventore. Quod eligendi, harum libero eveniet totam vel
+          aut reiciendis quidem necessitatibus, itaque delectus voluptatem
+          numquam aliquid perferendis! Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Reiciendis numquam delectus voluptatum inventore.
+          Quod eligendi, harum libero eveniet totam vel aut reiciendis quidem
+          necessitatibus, itaque delectus voluptatem numquam aliquid
+          perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Reiciendis numquam delectus voluptatum inventore. Quod eligendi, harum
+          libero eveniet totam vel aut reiciendis quidem necessitatibus, itaque
+          delectus voluptatem numquam aliquid perferendis! Lorem ipsum dolor sit
+          amet, consectetur adipisicing elit. Reiciendis numquam delectus
+          voluptatum inventore. Quod eligendi, harum libero eveniet totam vel
+          aut reiciendis quidem necessitatibus, itaque delectus voluptatem
+          numquam aliquid perferendis! Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Reiciendis numquam delectus voluptatum inventore.
+          Quod eligendi, harum libero eveniet totam vel aut reiciendis quidem
+          necessitatibus, itaque delectus voluptatem numquam aliquid
+          perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Reiciendis numquam delectus voluptatum inventore. Quod eligendi, harum
+          libero eveniet totam vel aut reiciendis quidem necessitatibus, itaque
+          delectus voluptatem numquam aliquid perferendis! Lorem ipsum dolor sit
+          amet, consectetur adipisicing elit. Reiciendis numquam delectus
+          voluptatum inventore. Quod eligendi, harum libero eveniet totam vel
+          aut reiciendis quidem necessitatibus, itaque delectus voluptatem
+          numquam aliquid perferendis! Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Reiciendis numquam delectus voluptatum inventore.
+          Quod eligendi, harum libero eveniet totam vel aut reiciendis quidem
+          necessitatibus, itaque delectus voluptatem numquam aliquid
+          perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Reiciendis numquam delectus voluptatum inventore. Quod eligendi, harum
+          libero eveniet totam vel aut reiciendis quidem necessitatibus, itaque
+          delectus voluptatem numquam aliquid perferendis! Lorem ipsum dolor sit
+          amet, consectetur adipisicing elit. Reiciendis numquam delectus
+          voluptatum inventore. Quod eligendi, harum libero eveniet totam vel
+          aut reiciendis quidem necessitatibus, itaque delectus voluptatem
+          numquam aliquid perferendis! Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Reiciendis numquam delectus voluptatum inventore.
+          Quod eligendi, harum libero eveniet totam vel aut reiciendis quidem
+          necessitatibus, itaque delectus voluptatem numquam aliquid
+          perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Reiciendis numquam delectus voluptatum inventore. Quod eligendi, harum
+          libero eveniet totam vel aut reiciendis quidem necessitatibus, itaque
+          delectus voluptatem numquam aliquid perferendis! Lorem ipsum dolor sit
+          amet, consectetur adipisicing elit. Reiciendis numquam delectus
+          voluptatum inventore. Quod eligendi, harum libero eveniet totam vel
+          aut reiciendis quidem necessitatibus, itaque delectus voluptatem
+          numquam aliquid perferendis! Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Reiciendis numquam delectus voluptatum inventore.
+          Quod eligendi, harum libero eveniet totam vel aut reiciendis quidem
+          necessitatibus, itaque delectus voluptatem numquam aliquid
+          perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Reiciendis numquam delectus voluptatum inventore. Quod eligendi, harum
+          libero eveniet totam vel aut reiciendis quidem necessitatibus, itaque
+          delectus voluptatem numquam aliquid perferendis! Lorem ipsum dolor sit
+          amet, consectetur adipisicing elit. Reiciendis numquam delectus
+          voluptatum inventore. Quod eligendi, harum libero eveniet totam vel
+          aut reiciendis quidem necessitatibus, itaque delectus voluptatem
+          numquam aliquid perferendis! Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Reiciendis numquam delectus voluptatum inventore.
+          Quod eligendi, harum libero eveniet totam vel aut reiciendis quidem
+          necessitatibus, itaque delectus voluptatem numquam aliquid
+          perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Reiciendis numquam delectus voluptatum inventore. Quod eligendi, harum
+          libero eveniet totam vel aut reiciendis quidem necessitatibus, itaque
+          delectus voluptatem numquam aliquid perferendis! Lorem ipsum dolor sit
+          amet, consectetur adipisicing elit. Reiciendis numquam delectus
+          voluptatum inventore. Quod eligendi, harum libero eveniet totam vel
+          aut reiciendis quidem necessitatibus, itaque delectus voluptatem
+          numquam aliquid perferendis! Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Reiciendis numquam delectus voluptatum inventore.
+          Quod eligendi, harum libero eveniet totam vel aut reiciendis quidem
+          necessitatibus, itaque delectus voluptatem numquam aliquid
+          perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Reiciendis numquam delectus voluptatum inventore. Quod eligendi, harum
+          libero eveniet totam vel aut reiciendis quidem necessitatibus, itaque
+          delectus voluptatem numquam aliquid perferendis! Lorem ipsum dolor sit
+          amet, consectetur adipisicing elit. Reiciendis numquam delectus
+          voluptatum inventore. Quod eligendi, harum libero eveniet totam vel
+          aut reiciendis quidem necessitatibus, itaque delectus voluptatem
+          numquam aliquid perferendis! Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Reiciendis numquam delectus voluptatum inventore.
+          Quod eligendi, harum libero eveniet totam vel aut reiciendis quidem
+          necessitatibus, itaque delectus voluptatem numquam aliquid
+          perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Reiciendis numquam delectus voluptatum inventore. Quod eligendi, harum
+          libero eveniet totam vel aut reiciendis quidem necessitatibus, itaque
+          delectus voluptatem numquam aliquid perferendis! Lorem ipsum dolor sit
+          amet, consectetur adipisicing elit. Reiciendis numquam delectus
+          voluptatum inventore. Quod eligendi, harum libero eveniet totam vel
+          aut reiciendis quidem necessitatibus, itaque delectus voluptatem
+          numquam aliquid perferendis! Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Reiciendis numquam delectus voluptatum inventore.
+          Quod eligendi, harum libero eveniet totam vel aut reiciendis quidem
+          necessitatibus, itaque delectus voluptatem numquam aliquid
+          perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Reiciendis numquam delectus voluptatum inventore. Quod eligendi, harum
+          libero eveniet totam vel aut reiciendis quidem necessitatibus, itaque
+          delectus voluptatem numquam aliquid perferendis! Lorem ipsum dolor sit
+          amet, consectetur adipisicing elit. Reiciendis numquam delectus
+          voluptatum inventore. Quod eligendi, harum libero eveniet totam vel
+          aut reiciendis quidem necessitatibus, itaque delectus voluptatem
+          numquam aliquid perferendis! Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Reiciendis numquam delectus voluptatum inventore.
+          Quod eligendi, harum libero eveniet totam vel aut reiciendis quidem
+          necessitatibus, itaque delectus voluptatem numquam aliquid
+          perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Reiciendis numquam delectus voluptatum inventore. Quod eligendi, harum
+          libero eveniet totam vel aut reiciendis quidem necessitatibus, itaque
+          delectus voluptatem numquam aliquid perferendis!
+        </main>
+      </div>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import ErrorPage from "~/pages/ErrorPage";
 import App from './App'
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import './index.css'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <ErrorPage/>
+  },
+  {
+    path: "/login",
+    element: <App/>,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
