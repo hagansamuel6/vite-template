cfb905464c09547ff337750cbb49795d2a7de130
and-rews <oseiandrews394@gmail.com>
2022-12-13 16:11:13 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,9 @@
-import Button from '@mui/material/Button';
+import Nav from "./Nav";
 
 export default function App() {
   return (
     <div className="flex">
-      <nav className="max-h-screen w-80 p-6 bg-blue-600 text-white overflow-auto hidden lg:block" >
-        <div className="h-16 w-full bg-orange-400 "></div>
-
-        <Button variant="contained">Hello World</Button>
-        
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. In nisi ex
-        eaque similique! Harum laudantium reiciendis placeat accusantium ab
-        asperiores perferendis cupiditate? Delectus debitis, ipsa molestias
-        accusamus iusto aliquam porro. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. In nisi ex eaque similique! Harum laudantium
-        reiciendis placeat accusantium ab asperiores perferendis cupiditate?
-        Delectus debitis, ipsa molestias accusamus iusto aliquam porro. Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. In nisi ex eaque
-        similique! Harum laudantium reiciendis placeat accusantium ab asperiores
-        perferendis cupiditate? Delectus debitis, ipsa molestias accusamus iusto
-        aliquam porro. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        In nisi ex eaque similique! Harum laudantium reiciendis placeat
-        accusantium ab asperiores perferendis cupiditate? Delectus debitis, ipsa
-        molestias accusamus iusto aliquam porro. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. In nisi ex eaque similique! Harum
-        laudantium reiciendis placeat accusantium ab asperiores perferendis
-        cupiditate? Delectus debitis, ipsa molestias accusamus iusto aliquam
-        porro. Lorem ipsum dolor sit amet consectetur adipisicing elit. In nisi
-        ex eaque similique! Harum laudantium reiciendis placeat accusantium ab
-        asperiores perferendis cupiditate? Delectus debitis, ipsa molestias
-        accusamus iusto aliquam porro. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. In nisi ex eaque similique! Harum laudantium
-        reiciendis placeat accusantium ab asperiores perferendis cupiditate?
-        Delectus debitis, ipsa molestias accusamus iusto aliquam porro. Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. In nisi ex eaque
-        similique! Harum laudantium reiciendis placeat accusantium ab asperiores
-        perferendis cupiditate? Delectus debitis, ipsa molestias accusamus iusto
-        aliquam porro. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        In nisi ex eaque similique! Harum laudantium reiciendis placeat
-        accusantium ab asperiores perferendis cupiditate? Delectus debitis, ipsa
-        molestias accusamus iusto aliquam porro. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. In nisi ex eaque similique! Harum
-        laudantium reiciendis placeat accusantium ab asperiores perferendis
-        cupiditate? Delectus debitis, ipsa molestias accusamus iusto aliquam
-        porro. Lorem ipsum dolor sit amet consectetur adipisicing elit. In nisi
-        ex eaque similique! Harum laudantium reiciendis placeat accusantium ab
-        asperiores perferendis cupiditate? Delectus debitis, ipsa molestias
-        accusamus iusto aliquam porro.
-      </nav>
+      <Nav />
 
       <div className="flex-1 max-h-screen top-0 right-0 overflow-auto relative">
         <header className="fixed h-16 w-full bg-green-600 p-6 mb-6">
---
+++ src/Nav.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+const Nav = () => {
+  return (
+    <div>
+      <nav className="w-80 p-6 bg-blue-600 text-white overflow-auto hidden lg:block">
+        <div className="h-16 w-full bg-orange-400 "></div>
+      </nav>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+      <p>g</p>
+    </div>
+  );
+};
+
+export default Nav;
