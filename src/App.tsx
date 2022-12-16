95a08108b4cf7a8c6e8704596b416b8cd5280eb0
and-rews <oseiandrews394@gmail.com>
2022-12-16 14:14:25 +0000

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,144 +1,220 @@
 import Nav from "./Nav";
+import "./Nav.css";
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import Badge from "@mui/material/Badge";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MailIcon from "@mui/icons-material/Mail";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MoreIcon from "@mui/icons-material/MoreVert";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
 
 export default function App() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="show 17 new notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={17} color="error">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
   return (
     <div className="flex" style={{ height: "100vh", overflowY: "hidden" }}>
       <Nav />
+      <Box sx={{ flexGrow: 1200 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ display: { xs: "none", sm: "block" } }}
+            >
+              Dashboard
+            </Typography>
+            <Search style={{ width: "50vw" }}>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ "aria-label": "search" }}
+              />
+            </Search>
+            <Box sx={{ flexGrow: 1 }} />
+            <Box sx={{ display: { xs: "none", md: "flex" } }}>
+              <IconButton
+                size="large"
+                edge="end"
+                aria-label="account of current user"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={handleProfileMenuOpen}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+            </Box>
+            <Box sx={{ display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="show more"
+                aria-controls={mobileMenuId}
+                aria-haspopup="true"
+                onClick={handleMobileMenuOpen}
+                color="inherit"
+              >
+                <MoreIcon />
+              </IconButton>
+            </Box>
+          </Toolbar>
+        </AppBar>
+        {renderMobileMenu}
+        {renderMenu}
+      </Box>
 
       <div
         className="flex-1 max-h-screen top-0 right-0 overflow-auto relative"
         style={{ display: "flex" }}
       >
-        <header className="fixed h-16 w-full bg-green-600 p-6 mb-6">
-          <div className="bg-gray-500">header</div>
-        </header>
-        <main className=" w-full bg-red-500 p-6 mt-12">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
-          numquam delectus voluptatum inventore. Quod eligendi, harum libero
-          eveniet totam vel aut reiciendis quidem necessitatibus, itaque
-          delectus voluptatem numquam aliquid perferendis! Lorem ipsum dolor sit
-          amet, consectetur adipisicing elit. Reiciendis numquam delectus
-          voluptatum inventore. Quod eligendi, harum libero eveniet totam vel
-          aut reiciendis quidem necessitatibus, itaque delectus voluptatem
-          numquam aliquid perferendis! Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Reiciendis numquam delectus voluptatum inventore.
-          Quod eligendi, harum libero eveniet totam vel aut reiciendis quidem
-          necessitatibus, itaque delectus voluptatem numquam aliquid
-          perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Reiciendis numquam delectus voluptatum inventore. Quod eligendi, harum
-          libero eveniet totam vel aut reiciendis quidem necessitatibus, itaque
-          delectus voluptatem numquam aliquid perferendis! Lorem ipsum dolor sit
-          amet, consectetur adipisicing elit. Reiciendis numquam delectus
-          voluptatum inventore. Quod eligendi, harum libero eveniet totam vel
-          aut reiciendis quidem necessitatibus, itaque delectus voluptatem
-          numquam aliquid perferendis! Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Reiciendis numquam delectus voluptatum inventore.
-          Quod eligendi, harum libero eveniet totam vel aut reiciendis quidem
-          necessitatibus, itaque delectus voluptatem numquam aliquid
-          perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Reiciendis numquam delectus voluptatum inventore. Quod eligendi, harum
-          libero eveniet totam vel aut reiciendis quidem necessitatibus, itaque
-          delectus voluptatem numquam aliquid perferendis! Lorem ipsum dolor sit
-          amet, consectetur adipisicing elit. Reiciendis numquam delectus
-          voluptatum inventore. Quod eligendi, harum libero eveniet totam vel
-          aut reiciendis quidem necessitatibus, itaque delectus voluptatem
-          numquam aliquid perferendis! Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Reiciendis numquam delectus voluptatum inventore.
-          Quod eligendi, harum libero eveniet totam vel aut reiciendis quidem
-          necessitatibus, itaque delectus voluptatem numquam aliquid
-          perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Reiciendis numquam delectus voluptatum inventore. Quod eligendi, harum
-          libero eveniet totam vel aut reiciendis quidem necessitatibus, itaque
-          delectus voluptatem numquam aliquid perferendis! Lorem ipsum dolor sit
-          amet, consectetur adipisicing elit. Reiciendis numquam delectus
-          voluptatum inventore. Quod eligendi, harum libero eveniet totam vel
-          aut reiciendis quidem necessitatibus, itaque delectus voluptatem
-          numquam aliquid perferendis! Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Reiciendis numquam delectus voluptatum inventore.
-          Quod eligendi, harum libero eveniet totam vel aut reiciendis quidem
-          necessitatibus, itaque delectus voluptatem numquam aliquid
-          perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Reiciendis numquam delectus voluptatum inventore. Quod eligendi, harum
-          libero eveniet totam vel aut reiciendis quidem necessitatibus, itaque
-          delectus voluptatem numquam aliquid perferendis! Lorem ipsum dolor sit
-          amet, consectetur adipisicing elit. Reiciendis numquam delectus
-          voluptatum inventore. Quod eligendi, harum libero eveniet totam vel
-          aut reiciendis quidem necessitatibus, itaque delectus voluptatem
-          numquam aliquid perferendis! Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Reiciendis numquam delectus voluptatum inventore.
-          Quod eligendi, harum libero eveniet totam vel aut reiciendis quidem
-          necessitatibus, itaque delectus voluptatem numquam aliquid
-          perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Reiciendis numquam delectus voluptatum inventore. Quod eligendi, harum
-          libero eveniet totam vel aut reiciendis quidem necessitatibus, itaque
-          delectus voluptatem numquam aliquid perferendis! Lorem ipsum dolor sit
-          amet, consectetur adipisicing elit. Reiciendis numquam delectus
-          voluptatum inventore. Quod eligendi, harum libero eveniet totam vel
-          aut reiciendis quidem necessitatibus, itaque delectus voluptatem
-          numquam aliquid perferendis! Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Reiciendis numquam delectus voluptatum inventore.
-          Quod eligendi, harum libero eveniet totam vel aut reiciendis quidem
-          necessitatibus, itaque delectus voluptatem numquam aliquid
-          perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Reiciendis numquam delectus voluptatum inventore. Quod eligendi, harum
-          libero eveniet totam vel aut reiciendis quidem necessitatibus, itaque
-          delectus voluptatem numquam aliquid perferendis! Lorem ipsum dolor sit
-          amet, consectetur adipisicing elit. Reiciendis numquam delectus
-          voluptatum inventore. Quod eligendi, harum libero eveniet totam vel
-          aut reiciendis quidem necessitatibus, itaque delectus voluptatem
-          numquam aliquid perferendis! Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Reiciendis numquam delectus voluptatum inventore.
-          Quod eligendi, harum libero eveniet totam vel aut reiciendis quidem
-          necessitatibus, itaque delectus voluptatem numquam aliquid
-          perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Reiciendis numquam delectus voluptatum inventore. Quod eligendi, harum
-          libero eveniet totam vel aut reiciendis quidem necessitatibus, itaque
-          delectus voluptatem numquam aliquid perferendis! Lorem ipsum dolor sit
-          amet, consectetur adipisicing elit. Reiciendis numquam delectus
-          voluptatum inventore. Quod eligendi, harum libero eveniet totam vel
-          aut reiciendis quidem necessitatibus, itaque delectus voluptatem
-          numquam aliquid perferendis! Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Reiciendis numquam delectus voluptatum inventore.
-          Quod eligendi, harum libero eveniet totam vel aut reiciendis quidem
-          necessitatibus, itaque delectus voluptatem numquam aliquid
-          perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Reiciendis numquam delectus voluptatum inventore. Quod eligendi, harum
-          libero eveniet totam vel aut reiciendis quidem necessitatibus, itaque
-          delectus voluptatem numquam aliquid perferendis! Lorem ipsum dolor sit
-          amet, consectetur adipisicing elit. Reiciendis numquam delectus
-          voluptatum inventore. Quod eligendi, harum libero eveniet totam vel
-          aut reiciendis quidem necessitatibus, itaque delectus voluptatem
-          numquam aliquid perferendis! Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Reiciendis numquam delectus voluptatum inventore.
-          Quod eligendi, harum libero eveniet totam vel aut reiciendis quidem
-          necessitatibus, itaque delectus voluptatem numquam aliquid
-          perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Reiciendis numquam delectus voluptatum inventore. Quod eligendi, harum
-          libero eveniet totam vel aut reiciendis quidem necessitatibus, itaque
-          delectus voluptatem numquam aliquid perferendis! Lorem ipsum dolor sit
-          amet, consectetur adipisicing elit. Reiciendis numquam delectus
-          voluptatum inventore. Quod eligendi, harum libero eveniet totam vel
-          aut reiciendis quidem necessitatibus, itaque delectus voluptatem
-          numquam aliquid perferendis! Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Reiciendis numquam delectus voluptatum inventore.
-          Quod eligendi, harum libero eveniet totam vel aut reiciendis quidem
-          necessitatibus, itaque delectus voluptatem numquam aliquid
-          perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Reiciendis numquam delectus voluptatum inventore. Quod eligendi, harum
-          libero eveniet totam vel aut reiciendis quidem necessitatibus, itaque
-          delectus voluptatem numquam aliquid perferendis! Lorem ipsum dolor sit
-          amet, consectetur adipisicing elit. Reiciendis numquam delectus
-          voluptatum inventore. Quod eligendi, harum libero eveniet totam vel
-          aut reiciendis quidem necessitatibus, itaque delectus voluptatem
-          numquam aliquid perferendis! Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Reiciendis numquam delectus voluptatum inventore.
-          Quod eligendi, harum libero eveniet totam vel aut reiciendis quidem
-          necessitatibus, itaque delectus voluptatem numquam aliquid
-          perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Reiciendis numquam delectus voluptatum inventore. Quod eligendi, harum
-          libero eveniet totam vel aut reiciendis quidem necessitatibus, itaque
-          delectus voluptatem numquam aliquid perferendis!
-        </main>
+        <main className=" w-full p-6 mt-12"></main>
       </div>
     </div>
   );
--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,60 +1,120 @@
-import React from "react";
-import Button from "@mui/material/Button";
 import "./Nav.css";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import GroupsIcon from "@mui/icons-material/Groups";
+import AssignmentTurnedInIcon from "@mui/icons-material/AssignmentTurnedIn";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import AssessmentIcon from "@mui/icons-material/Assessment";
+import PaymentIcon from "@mui/icons-material/Payment";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import PersonIcon from "@mui/icons-material/Person";
+import { Avatar } from "@mui/material";
+import { deepOrange } from "@mui/material/colors";
 
 const Nav = () => {
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <nav className="w-80 p-6 bg-blue-600 text-white overflow-auto hidden lg:block">
-        <div className="h-16 w-full bg-orange-400 "></div>
+    <div
+      className="bg-blue-900"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        color: "white",
+      }}
+    >
+      <nav
+        className="w-80 text-white hidden lg:block"
+        style={{
+          boxShadow:
+            "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
+        }}
+      >
+        <div className="h-16 w-full bg-blue-900 nav">
+          <div>
+            <Avatar
+              sx={{ bgcolor: deepOrange[500] }}
+              alt="Andrews"
+              src="/broken-image.jpg"
+            >
+              A
+            </Avatar>
+          </div>
+          <div className="name">Andrews</div>
+        </div>
       </nav>
+      <h2 className="h2">TLG TRANSPORT</h2>
       <div
         style={{
           overflowY: "auto",
-          backgroundColor: "yellow",
           flex: "1",
         }}
       >
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
-        <p>g</p>
+        <div className="items">
+          <ul>
+            <li className="list-item">
+              <div className="icon">
+                <DashboardIcon />
+              </div>
+              <div>
+                <a href="/">Dashboard</a>
+              </div>
+            </li>
+            <li className="list-item">
+              <div className="icon">
+                <GroupsIcon />
+              </div>
+              <div>
+                <a href="/">Staff</a>
+              </div>
+            </li>
+            <li className="list-item">
+              <div className="icon">
+                <AssignmentTurnedInIcon />
+              </div>
+              <div>
+                <a href="/">Task</a>
+              </div>
+            </li>
+            <li className="list-item">
+              <div className="icon">
+                <AccountBalanceWalletIcon />
+              </div>
+              <div>
+                <a href="/">Accounts</a>
+              </div>
+            </li>
+            <li className="list-item">
+              <div className="icon">
+                <AssessmentIcon />
+              </div>
+              <div>
+                <a href="/">Reports</a>
+              </div>
+            </li>
+            <li className="list-item">
+              <div className="icon">
+                <AttachMoneyIcon />
+              </div>
+              <div>
+                <a href="/">Salary</a>
+              </div>
+            </li>
+            <li className="list-item">
+              <div className="icon">
+                <PaymentIcon />
+              </div>
+              <div>
+                <a href="/">Payments</a>
+              </div>
+            </li>
+            <li className="list-item">
+              <div className="icon">
+                <PersonIcon />
+              </div>
+              <div>
+                <a href="/">Users</a>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
